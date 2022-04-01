5aea91262057c7164668b60bc12dd8ca3104a0cb
sergeyblinov36 <sergeyblinov36@gmail.com>
2022-04-01 22:49:03 +0300

    works with mongo

---
+++ config.py
@@ -0,0 +1,3 @@
+# mongoConnection = "mongodb+srv://sergey:17041993@cluster0.51zvw.mongodb.net/EyeSave?retryWrites=true&w=majority"
+# mongoConnection = "mongodb://localhost:27017/EyeSave"
+mongoConnection = "mongodb+srv://ayaari:adiyaari1989@cluster0.yz1bi.mongodb.net/EyeSAVE_DB?retryWrites=true&w=majority"
