3a09545d1a20c6ac1a778ef4c41609eb216780e8
sergeyblinov36 <48134651+sergeyblinov36@users.noreply.github.com>
2022-08-04 14:56:30 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-# mongoConnection = "mongodb+srv://sergey:17041993@cluster0.51zvw.mongodb.net/EyeSave?retryWrites=true&w=majority"
-# mongoConnection = "mongodb://localhost:27017/EyeSave"
-# mongoConnection = "mongodb+srv://ayaari:adiyaari1989@cluster0.yz1bi.mongodb.net/EyeSAVE_DB?retryWrites=true&w=majority"
